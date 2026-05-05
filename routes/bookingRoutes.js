59616827ec1569ac1d17e36e6fd0c6b253bb4aab
Shabbeer <shabbeer@Shabbeers-MacBook-Air.local>
2026-05-05 16:14:26 +0530

    sitevisit updated status converted

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -24,7 +24,7 @@ router.get("/", fetchuser, async (req, res) => {
       .populate("agent", "name phone")
       .populate("location", "name")
       .populate("colony", "name")
-      .populate("plot", "plotNumber");
+      // .populate("plot", "plotNumber");
 
     res.json(bookings);
   } catch (error) {
